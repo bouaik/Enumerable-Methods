6d9ce47b7720b4367ef750dc0aa56228c534fefe
bouaik <bouaik.lhou@outlook.fr>
2020-03-02 12:34:24 +0100

    add linter files

--- index.rb
+++ index.rb
@@ -1,14 +1,15 @@
 module Enumerable
-    puts "<------ MY EACH ------>"
-    def my_each
-        return "no block given" unless block_given?
-        0.upto(self.length - 1) do |i|
-            yield self[i]
-        end
-        self
+  puts '<------ MY EACH ------>'
+  def my_each
+    return 'no block given' unless block_given?
+
+    0.upto(length - 1) do |i|
+      yield self[i]
     end
-    x = [1, 5,-4, 3, 89, 11, -35]
-    x.my_each do |ele|
-        puts ele * 2
-    end
+    self
+  end
+  x = [1, 5, -4, 3, 89, 11, -35]
+  x.my_each do |ele|
+    puts ele * 2
+  end
 end
