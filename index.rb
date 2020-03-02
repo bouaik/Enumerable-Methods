f33a9f82c44bcd637d103140dc612da9955b0a29
bouaik <bouaik.lhou@outlook.fr>
2020-03-02 13:50:43 +0100

    make my select enumerable

--- index.rb
+++ index.rb
@@ -20,6 +20,18 @@ module Enumerable
     end
     self
   end
+
+  def my_select
+    array = []
+    if block_given?
+      0.upto(length - 1) do |i|
+        array << self[i] if yield self[i]
+      end
+    else
+      puts 'no block is given'
+    end
+    array
+  end
 end
 
 x = [1, 5, -4, 3, 89, 11, -35]
@@ -28,8 +40,14 @@ puts '<------ MY EACH ------>'
 x.my_each do |ele|
   puts ele * ele
 end
+puts
 
 puts '<------ MY EACH WITH INDEX ------>'
 x.my_each_with_index do |ele, index|
   puts ele * index
 end
+puts
+
+puts '<------ MY SELECT ------>'
+print x.my_select(&:positive?)
+puts
