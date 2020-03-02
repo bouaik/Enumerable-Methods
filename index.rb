f91200ee8f1db9306b4032c925f1edd3ecf24e10
bouaik <bouaik.lhou@outlook.fr>
2020-03-02 14:42:37 +0100

    make my all enumerable

--- index.rb
+++ index.rb
@@ -32,22 +32,39 @@ module Enumerable
     end
     array
   end
+
+  def my_all
+    value = true
+    if block_given?
+      0.upto(length - 1) do |i|
+        value = false unless yield self[i]
+      end
+    else
+      puts 'no block is given'
+    end
+    value
+  end
 end
 
 x = [1, 5, -4, 3, 89, 11, -35]
+words = %w[bacon orang apple]
 
 puts '<------ MY EACH ------>'
 x.my_each do |ele|
   puts ele * ele
 end
-puts
 
 puts '<------ MY EACH WITH INDEX ------>'
 x.my_each_with_index do |ele, index|
   puts ele * index
 end
-puts
 
 puts '<------ MY SELECT ------>'
 print x.my_select(&:positive?)
 puts
+
+puts '<------ MY ALL ------>'
+words.my_all do |str|
+  str.size == 5
+end
+x.my_all(&:positive?)
