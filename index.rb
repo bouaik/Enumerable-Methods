8261b3c53883806b62b19e30b44e8453867781e5
bouaik <bouaik.lhou@outlook.fr>
2020-03-02 13:01:01 +0100

    make my each with index enumerable

--- index.rb
+++ index.rb
@@ -1,5 +1,4 @@
 module Enumerable
-  puts '<------ MY EACH ------>'
   def my_each
     if block_given?
       0.upto(length - 1) do |i|
@@ -10,10 +9,27 @@ module Enumerable
     end
     self
   end
+
+  def my_each_with_index
+    if block_given?
+      0.upto(length - 1) do |i|
+        yield(self[i], i)
+      end
+    else
+      puts 'no block is given'
+    end
+    self
+  end
 end
 
 x = [1, 5, -4, 3, 89, 11, -35]
 
+puts '<------ MY EACH ------>'
 x.my_each do |ele|
   puts ele * ele
+end
+
+puts '<------ MY EACH WITH INDEX ------>'
+x.my_each_with_index do |ele, index|
+  puts ele * index
 end
