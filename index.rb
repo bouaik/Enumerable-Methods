d748fa536085760f9ad6130450a40e1786cf066b
bouaik <bouaik.lhou@outlook.fr>
2020-03-04 10:54:12 +0100

    add to_enum to my_each, my_each_with_index and my_select

--- index.rb
+++ index.rb
@@ -1,34 +1,28 @@
 module Enumerable # rubocop:disable Metrics/ModuleLength
   def my_each
-    if block_given?
-      0.upto(length - 1) do |i|
-        yield self[i]
-      end
-    else
-      puts 'no block is given'
+    return to_enum unless block_given?
+
+    0.upto(length - 1) do |i|
+      yield self[i]
     end
     self
   end
 
   def my_each_with_index
-    if block_given?
-      0.upto(length - 1) do |i|
-        yield(self[i], i)
-      end
-    else
-      puts 'no block is given'
+    return to_enum unless block_given?
+
+    0.upto(length - 1) do |i|
+      yield(self[i], i)
     end
     self
   end
 
   def my_select
     array = []
-    if block_given?
-      0.upto(length - 1) do |i|
-        array << self[i] if yield self[i]
-      end
-    else
-      puts 'no block is given'
+    return to_enum unless block_given?
+
+    0.upto(length - 1) do |i|
+      array << self[i] if yield self[i]
     end
     array
   end
@@ -120,61 +114,6 @@ module Enumerable # rubocop:disable Metrics/ModuleLength
   end
 end
 
-x = [1, 5, -4, 3, 89, 11, -35]
-words = %w[bacon orang apple]
-my_proc = proc { |ele| ele * 2 }
-
-puts '<------ MY EACH ------>'
-x.my_each do |ele|
-  puts ele * ele
-end
-
-puts '<------ MY EACH WITH INDEX ------>'
-x.my_each_with_index do |ele, index|
-  puts ele * index
-end
-
-puts '<------ MY SELECT ------>'
-print x.my_select(&:positive?)
-puts
-
-puts '<------ MY ALL ------>'
-words.my_all do |str|
-  str.size == 5
-end
-x.my_all(&:positive?)
-
-puts '<------ MY NONE ------>'
-words.my_none do |str|
-  str.size == 5
-end
-x.my_none(&:positive?)
-
-puts '<------ MY ANY ------>'
-words.my_any do |str|
-  str.size == 5
-end
-x.my_any(&:positive?)
-
-puts '<------ MY COUNT ------>'
-words.my_count do |str|
-  str.size == 5
-end
-x.my_count(&:positive?)
-
-puts '<------ MY MAP ------>'
-x.my_map(my_proc)
-puts
-x.my_map { |ele| ele * 2 }
-
-puts '<------ MY INJECT ------>'
-x.my_inject(4) do |a, b|
-  a + b
-end
-
-puts '<------ TEST MY INJECT WITH A METHOD ------>'
 def multiply_els(arr)
   arr.my_inject { |a, b| a * b }
 end
-
-multiply_els([2, 4, 5])
